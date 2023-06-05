d5bd579008d2a19d2895c52f35135829717c1dc8
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-06-05 13:18:49 +0530

    menu items added

--- menu/app.js
+++ menu/app.js
@@ -85,9 +85,28 @@ const sectionCenter = document.querySelector(".section-center");
 
 window.addEventListener('DOMContentLoaded', function(){
   // console.log("shake and bake");
-  let displayMenu = menu.map(function(item){
-    console.log(item);
+  displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
+    // console.log(item);
+
+    return `<article class="menu-item">
+    <img src= ${item.img} class="photo" alt=${item.title} />
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">
+      ${item.desc}
+      </p>
+    </div>
+  </article>`;
 
   });
-  console.log(displayMenu);
-});
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML=displayMenu;
+  
+}
