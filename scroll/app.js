a563bb9ffabb142df6b5f01fe7131abdbfb0a7b4
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-06-08 10:35:10 +0530

    added functionalities

--- scroll/app.js
+++ scroll/app.js
@@ -4,10 +4,50 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById('date');
+date.innerHTML=new Date().getFullYear();
 
 // ********** close links ************
+const linksHolder = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+const navToggle = document.querySelector(".nav-toggle");
+
+navToggle.addEventListener('click', function(){
+    // linksHolder.classList.toggle("show-links");
+
+    const containerHeight = linksHolder.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    
+    if(containerHeight === 0){
+        linksHolder.style.height = `${linksHeight}px`;
+    }
+    else{
+        linksHolder.style.height = 0;
+    }
+});
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 
 // ********** fixed navbar ************
+window.addEventListener('scroll', function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav');
+    }
+    else{
+        navbar.classList.remove('fixed-nav');
+    }
+
+    if(scrollHeight > 500){
+        topLink.classList.add('show-link');
+    }
+    else{
+        topLink.classList.remove('show-link');
+    }
+});
 
 // ********** smooth scroll ************
 // select links
