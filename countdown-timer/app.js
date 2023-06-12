bc25f682b693e475d9b002dc2d84a4707c3043b0
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-06-12 21:01:15 +0530

    setup completed working on js

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -21,3 +21,20 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+console.log(items);
+let futureDate = new Date(2023, 6, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+// const seconds = futureDate.getSeconds();
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+giveaway.textContent = `giveaway ends on ${date} ${month} ${year} ${hours}:${minutes}am`;
