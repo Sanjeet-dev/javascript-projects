76bc0d79b9915784d8e2c89f08516b83b2fbbf6c
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-06-16 10:59:27 +0530

    final commit

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -26,8 +26,13 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
-let futureDate = new Date(2023, 6, 24, 11, 30, 0);
+// let futureDate = new Date(2023, 6, 15, 9, 30, 0);
+const futureDate = new Date(tempYear,tempMonth, tempDay+10, 11, 30, 0)
 
 
 const year = futureDate.getFullYear();
@@ -69,12 +74,26 @@ function getRemainingTime() {
   let seconds = Math.floor((t % oneMinute) / 1000);
 
   // set values array
-  const values = [days,hours,minutes,seconds];
+  const values = [days, hours, minutes, seconds];
 
-  items.forEach(function(item,index){
-    item.innerHTML = values[index]
+  function format(item) {
+    if (item < 10) {
+      return item = `0${item}`
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
   });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML= `<h4 class="expired">Sorry, this giveaway has expired</h4>`
+
+  }
 
 
 }
+//countdown
+let countdown = setInterval(getRemainingTime, 1000);
 getRemainingTime();
