b3929b223ac7cd17de29c9b9164efc7c8be90bea
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-06-21 21:46:29 +0530

    form and container added

--- grocery/app.js
+++ grocery/app.js
@@ -19,22 +19,63 @@ let editID = "";
 form.addEventListener('submit', addItem)
 
 // ****** FUNCTIONS **********
-function addItem(e){
+function addItem(e) {
     e.preventDefault();
     const value = grocery.value;
     const id = new Date().getTime().toString();
-    if(value !=='' && editFlag === false){
-        console.log('add item to the list');
+
+    if (value && !editFlag) {
+        const element = document.createElement('artcle');
+        // and class
+        element.classList.add('grocery-item');
+        // add id
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = ` <p class="title">${value}</p>
+        <div class="btn-container">
+          <button type="button" class="edit-btn">
+            <i class="fas fa-edit"></i>
+          </button>
+          <button type="button"  class="delete-btn">
+            <i class="fas fa-trash"></i>
+          </button>
+        </div>`;
+        // append child
+        list.appendChild(element);
+        //display alert
+        displayAlert('item add to the list', 'success');
+        // show container
+        container.classList.add("show-container");
+        // add to local storage
+        addToLocalStorage(id,value);
     }
-    else if(value!=='' && editFlag === true){
+    else if (value && editFlag) {
         console.log('editing');
     }
-    else{
-        console.log('empty value')
+    else {
+        displayAlert('please enter value', 'danger');
     }
 
 }
+//
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    //remove alert
+    setTimeout(function () {
+        alert.textContent = '';
+        alert.classList.remove(`alert-${action}`);
+
+
+    }, 1000);
+}
+// set back to default
 
 // ****** LOCAL STORAGE **********
+function addToLocalStorage(id,value){
+    console.log('added to local storage');
+}
 
 // ****** SETUP ITEMS **********
